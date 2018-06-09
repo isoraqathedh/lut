1e0c8cf0799d5d7cfd814027a58c36e995537b9e
Isoraqathedh Zorethan <isoraqathedh.zorethan@gmail.com>
2018-06-09 01:42:07 +0100

    Refine the configuration of the LUT object b allowing extra values.

--- utau-write.lisp
+++ utau-write.lisp
@@ -19,26 +19,34 @@
    (note-counter :initform 0
                  :accessor note-counter)
    (key-signature :initform "1 = C4"
-                  :initarg :key-signature)
+                  :initarg :key-signature
+                  :accessor key-signature)
    (time-signature :initform #(4 4)
-                   :initarg :time-signature)
+                   :initarg :time-signature
+                   :accessor time-signature)
    (kana-romanisation :initform nil
-                      :initarg :kana-romanisation))
+                      :initarg :kana-romanisation
+                      :reader kana-romanisation))
   (:documentation "Internal representation of an LUT file"))
+
+(defgeneric dump-to-stream (lut-file stream)
+  (:documentation "Write the parsed configuration into a stream.")
+  (:method ((lut-file lut-file) (stream stream))
+    (with-accessors ((contents contents)
+                     (version version)) lut-file
+      (format stream "[#VERSION]~aUST Version~a~a" *crlf* version *crlf*)
+      (write-stream contents stream)
+      (format stream "[#TRACKEND]~a" *crlf*))))
 
 (defgeneric dump-file (lut-file)
   (:documentation "Write the parsed configuration into the file.")
   (:method ((lut-file lut-file))
-    (with-accessors ((filename filename)
-                     (contents contents)
-                     (version version))
-        lut-file
-      (with-open-file (file filename
-                            :direction :output
-                            :external-format :shift_jis)
-        (format file "[#VERSION]~aUST Version~a~a" *crlf* version *crlf*)
-        (write-stream contents file)
-        (format file "[#TRACKEND]~a" *crlf*)))))
+    (with-open-file (file (filename lut-file)
+                          :direction :output
+                          :external-format :shift_jis)
+      (dump-to-stream lut-file file))))
+
+(defgeneric dump-stream (stream lut-file))
 
 (defgeneric create-setting-section (lut-file options)
   (:documentation "Create the setting section of the UST file.")
@@ -49,18 +57,51 @@
         (dolist (i options)
           (set-option contents setting-header (car i) (cdr i)))))))
 
-(defun set-up-file (name tempo voice-name &key
-                                            (version "1.2")
-                                            (insert-voice-dir t)
-                                            (file-dir (uiop:getcwd))
-                                            (tracks 1)
-                                            (alternate-name name))
+(defmacro ensure-type (accessor-form &body acceptable-and-convertible-types)
+  "Ensure that the item named in ACCESSOR-FORM is of the correct type.
+
+To do this, it analyses acceptable-and-convertable-type.
+There are two possible forms:
+
+- A single symbol, which is an acceptable type. Nothing will be done.
+- A list of the form (TYPE CONVERTER), which is a convertible type.
+  In this case CONVERTER will be called against ACCESSOR-FORM,
+  and the results will be SETFed back into ACCESSOR-FORM.
+- Any other type will signal an error."
+  `(etypecase ,accessor-form
+     ,@(loop for i in acceptable-and-convertible-types
+             if (listp i)
+             collect `(,(first i)
+                       (setf ,accessor-form (,(second i) ,accessor-form)))
+             else collect `(,i))))
+
+(defun set-up-file (name tempo voice-name
+                    &key
+                      (version "1.2")
+                      (insert-voice-dir t)
+                      (file-dir (uiop:getcwd))
+                      (tracks 1)
+                      (alternate-name name)
+                      kana-romanisation
+                      (key-signature (parse-key-signature "1 = C"))
+                      (time-signature #(4 4)))
+  "Create a LUT file object, and load set-up information into it."
   (let ((object (make-instance
                  'lut-file
                  :filename (make-pathname :name name
                                           :type "ust"
                                           :defaults file-dir)
+                 :kana-romanisation kana-romanisation
+                 :time-signature time-signature
+                 :key-signature key-signature
                  :version version)))
+    (ensure-type (key-signature object)
+      key-signature
+      (string parse-key-signature))
+    (ensure-type (time-signature object)
+      vector
+      null
+      (string parse-time-signature))
     (create-setting-section
      object (list (cons "Tempo" tempo)
                   (cons "Tracks" tracks)

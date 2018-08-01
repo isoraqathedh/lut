48427e2c0cf75cf90b08c4badd8a117937ae5725
Isoraķatheð Zorethan <isoraqathedh.zorethan@gmail.com>
2018-08-01 22:26:30 +0800

    Complete rewrite.

---
+++ utau-write-2.lisp
@@ -0,0 +1,23 @@
+;;;; utau-write.lisp
+;;; This file handles writing a completed file into a file.
+;;; This relies on the config being completed
+;;; prior to sending off to the functions in this file.
+
+(in-package #:utau-write)
+
+(defclass lut-file (note-collection)
+  ((properties :accessor properties
+               :initarg :properties
+               :initform (make-hash-table)))
+  (:documentation "A representation of a lut-file.
+
+This turns out to be just a note collection with extra details."))
+
+(defgeneric generate-config (lut-file)
+  (:documentation "Generate the output format from the data stream in"))
+
+(defgeneric dump-to-stream (generated-config stream)
+  (:documentation "Write the generated output file to a stream."))
+
+(defgeneric dump-to-file (lut-file)
+  (:documentation "Generate the output and write the file to stream."))

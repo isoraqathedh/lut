1ba4be4b34c4ccb5a071213ff1907c64495fe775
Isoraķatheð Zorethan <isoraqathedh.zorethan@gmail.com>
2018-08-02 21:46:01 +0800

    Start porting old things over and adding more stub functions.

--- note-collections.lisp
+++ note-collections.lisp
@@ -3,4 +3,28 @@
 ;;; These can be as simple as a list free of structure
 ;;; or a measure with a specified length
 ;;; or a bunch of measures for the purposes of repetition.
-;;; A number 
+
+(defclass note-collection ()
+  ((store
+    :accessor note-store
+    :initform ()))
+  (:documentation "A collection of notes."))
+
+(defclass measure (note-collection)
+  ((intended-length
+    :accessor intended-length
+    :initform nil
+    :initarg :intended-length
+    :documentation "The intended length of the measure in quarter-notes,
+or nil for no specific length."))
+  (:documentation "A note-collection that is also a measure."))
+
+(defgeneric add-note (note-collection note)
+  (:documentation "Add a note "))
+
+(defgeneric drop-note (note-collection)
+  (:documentation "Drop "))
+
+(defgeneric collection-length (note-collection))
+
+(defgeneric measure-full-p (measure))
--- utau-write-2.lisp
+++ utau-write-2.lisp
@@ -10,11 +10,41 @@
                :initarg :properties
                :initform (make-hash-table))
    (variable-store :accessor variable-store
-                   :initform (make-hash-table)))
+                   :initform (make-hash-table))
+   (version
+    :initform "1.2"
+    :initarg :version
+    :accessor version
+    :documentation "The UTAU version that the file requires.")
+   (key-signature
+    :initform "1 = C4"
+    :initarg :key-signature
+    :accessor key-signature
+    :documentation "A selected key signature of the song.")
+   (time-signature
+    :initform #(4 4)
+    :initarg :time-signature
+    :accessor time-signature
+    :documentation "A selected time signature of the song.")
+   (kana-romanisation
+    :initform nil
+    :initarg :kana-romanisation
+    :reader kana-romanisation
+    :documentation
+    "The setting for transforming romaji into kana."))
   (:documentation "A representation of a lut-file.
 
 This turns out to be just a note collection with extra details."))
 
+(defgeneric create-config (lut-file)
+  (:documentation "Set up a new output.")
+  (:method ((lut-file lut-file))
+    (let ((config-file (make-config :section-name-transform-fn #'string-upcase
+                                    :option-name-transform-fn #'identity)))
+      (add-section config-file "#SETTING")
+      )))
+
+;;; Final compiling and output
 (defgeneric generate-config (lut-file)
   (:documentation "Generate the output format from the data stream in"))
 

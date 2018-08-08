7b2ee81467e7e104a96e4b2c4112ff829ab15a9c
Isoraķatheð Zorethan <isoraqathedh.zorethan@gmail.com>
2018-08-08 23:22:35 +0800

    More details.

--- utau-write-2.lisp
+++ utau-write-2.lisp
@@ -8,24 +8,40 @@
 (defclass lut-file (note-collection)
   ((properties :accessor properties
                :initarg :properties
-               :initform (make-hash-table))
+               :initform (make-instance 'lut-settings))
+   (note-counter :accessor note-counter
+                 :initform 0)
    (variable-store :accessor variable-store
                    :initform (make-hash-table)))
   (:documentation "A representation of a lut-file.
 
 This turns out to be just a note collection with extra details."))
 
-(defgeneric create-config (lut-file)
-  (:documentation "Set up a new output.")
-  (:method ((lut-file lut-file))
-    (let ((config-file (make-config :section-name-transform-fn #'string-upcase
-                                    :option-name-transform-fn #'identity)))
-      (add-section config-file "#SETTING")
-      )))
+;;; Define translation of notes to the config system.
+(defgeneric append-to-file (config-file thing lut-variables)
+  (:documentation "Add the THING to the CONFIG-FILE.")
+  (:method ((config-file config) (thing lut-settings) lut-variables)
+    (add-section config-file "#SETTING")
+    ;; guff...
+    )
+  (:method ((config-file config) (thing note) lut-variables)
+    (add-section config-file ;; more guff...
+                 ))
+  (:method ((config-file config) (thing variable) (lut-variables lut-variable-store))
+    ())
+  (:method ((config-file config) (thing note-collection))
+    (loop for i in (get-notes note-collection)
+          do (append-to-file config-file thing))))
 
 ;;; Final compiling and output
 (defgeneric generate-config (lut-file)
-  (:documentation "Generate the output format from the data stream in"))
+  (:documentation "Generate the output format from the lut-file.")
+  (:method ((lut-file lut-file))
+    (let ((config-file (make-config :section-name-transform-fn #'string-upcase
+                                    :option-name-transform-fn #'identity)))
+      (append-to-file config-file (lut-settings lut-file))
+      (loop for i in (get-notes lut-file)
+            do (append-to-file config-file )))))
 
 (defgeneric dump-to-stream (generated-config stream)
   (:documentation "Write the generated output file to a stream."))
